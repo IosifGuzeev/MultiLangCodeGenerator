d58dc2d37c15c29097750f9db17fbd6742555c10
IosifGuzeev <Iosif.Guzeev@perfectart.com>
2020-05-05 17:51:52 +0700

    Done Cpp Factory

--- Factories/CppFactory/cppfactory.cpp
+++ Factories/CppFactory/cppfactory.cpp
@@ -1,6 +1,11 @@
 #include "cppfactory.h"
 
-CppFactory::CppFactory()
+std::shared_ptr<ClassUnit> CppFactory::GetClassUnit(std::string name, unsigned int flags)
 {
+    return std::shared_ptr<CppClassUnit>(new CppClassUnit(name));
+}
 
+std::shared_ptr<MethodUnit> CppFactory::GetMethodUnit(std::string name, std::string returnType, unsigned int flags)
+{
+    return std::shared_ptr<CppMethodUnit>(new CppMethodUnit(name, returnType));
 }
--- Factories/CppFactory/cppfactory.h
+++ Factories/CppFactory/cppfactory.h
@@ -1,17 +1,18 @@
 #ifndef CPPFACTORY_H
 #define CPPFACTORY_H
 #include<Interfaces.h>
-
+#include<Factories/CppFactory/cppclassunit.h>
+#include<Factories/CppFactory/cppmethodunit.h>
 
 class CppFactory:AbstractFactory
 {
 public:
-    CppFactory();
+    CppFactory(){};
 
     // AbstractFactory interface
 public:
-    std::shared_ptr<ClassUnit> GetClassUnit(std::string name, unsigned int flags) = 0;
-    std::shared_ptr<MethodUnit> GetMethodUnit(std::string name, std::string returnType, unsigned int flags) = 0;
+    std::shared_ptr<ClassUnit> GetClassUnit(std::string name, unsigned int flags) override;
+    std::shared_ptr<MethodUnit> GetMethodUnit(std::string name, std::string returnType, unsigned int flags) override;
 };
 
 #endif // CPPFACTORY_H
