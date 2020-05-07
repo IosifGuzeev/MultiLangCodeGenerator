e2801ec9e6ddea324c5aca37a50a01741fa6451e
IosifGuzeev <Iosif.Guzeev@perfectart.com>
2020-05-07 18:36:46 +0700

    Added tests run in main file

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <QCoreApplication>
+#include <codefactory.h>
+#include <Utilities/writers.h>
+#include <Tests/cpptest.h>
+#include <Tests/javatest.h>
+#include <Tests/csharptest.h>
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
-
-    return a.exec();
+    auto consoleWriter = new ConsoleWriter();
+    CppTest::RunTests(consoleWriter);
+    JavaTest::RunTests(consoleWriter);
+    CSharpTest::RunTests(consoleWriter);
 }
